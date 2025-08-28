7158de048da0264262971972c1aa507185dcabbd
Mikael Renvoise <klimae@icloud.com>
2025-08-28 17:20:11 +0200

    2 commit avec corrections

---
+++ src/data/dishes.js
@@ -0,0 +1,25 @@
+// Données des plats pour le carousel
+export const dishes = [
+  {
+    id: 1,
+    name: "Filet de Bœuf Wellington",
+    image: "/images/plats/beef-wellington.jpg",
+    description: "Filet de bœuf en croûte feuilletée, duxelles de champignons",
+    price: "42€"
+  },
+  {
+    id: 2,
+    name: "Saumon Mi-Cuit",
+    image: "/images/plats/salmon.jpg",
+    description: "Saumon mi-cuit, risotto aux asperges vertes",
+    price: "28€"
+  },
+  {
+    id: 3,
+    name: "Soufflé au Grand Marnier",
+    image: "/images/plats/souffle.jpg",
+    description: "Soufflé traditionnel au Grand Marnier, glace vanille",
+    price: "16€"
+  }
+  // Ajouter d'autres plats selon vos besoins
+];
